228e66826eb5ea47dd05e284c5765b6b8d4d531f
Dopesky <oboke69@gmail.com>
2018-08-30 01:46:03 +0300

    nn

---
+++ HomePage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<title>HomePage</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="MainCSS.css">
+	<link rel="stylesheet" type="text/css" href="PhoneMainCSS.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<nav class="navbar bg-info navbar-light navbar-expand-sm" style="border-radius: 5px;">
+			<a class="navbar-brand" href=""><img src="background.jpg" alt="Logo" style="width:40px;"></a>
+
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#smallScreen" style="outline: none;">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="smallScreen">
+				<ul class="navbar-nav">
+			      <li class="nav-item">
+			        <a class="nav-link text-light active" href="">Home</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link text-light" href="ContactsPage.php">Contacts</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link text-light" href="AboutPage.php">About</a>
+			      </li> 
+			      <li class="nav-item">
+			        <a class="nav-link text-light" href="HelpPage.php">Help</a>
+			      </li> 
+			    </ul>
+			</div>
+		</nav>
+	</div>
+</body>
+</html>
