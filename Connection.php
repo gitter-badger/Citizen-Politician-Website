16f4aa03259bf4eb6dbcf3385b167814483662bd
Dopesky <oboke69@gmail.com>
2018-09-05 01:17:52 +0300

    Testing PostgreSQL

--- Connection.php
+++ Connection.php
@@ -1,5 +1,5 @@
 <?php
-	$connection=mysqli_connect("localhost","dqhdzzkgptmtrc","d519e14a854b4182416f6b039dbb7f22cea904c0a78ae57394816d8c65cd1619","public");
+	$connection=mysqli_connect("localhost","dqhdzzkgptmtrc","d519e14a854b4182416f6b039dbb7f22cea904c0a78ae57394816d8c65cd1619");
 	if (!$connection) {
 		die("<br><br>Cannot Connect to Database Thus Site is Shutting Down.<br><br>".mysqli_connect_error());
 	}
