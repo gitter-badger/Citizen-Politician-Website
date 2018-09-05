fd9b97092bf53435b14f2dfba30733d1afd337ce
Dopesky <oboke69@gmail.com>
2018-09-05 06:44:24 +0300

    Removed unnecessary scripts and links.

--- Connection.php
+++ Connection.php
@@ -1,5 +1,5 @@
 <?php
-	$connection=mysqli_connect("sql7.freemysqlhosti.net","sql7255071","biggie5941","sql7255071");
+	$connection=mysqli_connect("sql7.freemysqlhosting.net","sql7255071","biggie5941","sql7255071");
 	//$connection=mysqli_connect("localhost","root","biggie5941","Citizen-Politician Website");
 	if ($connection) {
 		return;
@@ -19,16 +19,9 @@
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="MainCSS.css">
-	<link rel="stylesheet" type="text/css" href="PhoneMainCSS.css">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<script src="MainJS.js"></script>
 </head>
-<body style="background-color: lavender;background-image: none;">
+<body style="background-color: lavender;">
 	<br><br>
 	<div class="container alert alert-danger">
 		<h1 class="jumbotron text-secondary"><i class="fas fa-exclamation-triangle"></i> Error Establishing Connection to database. <i class="fas fa-exclamation-triangle"></i></h1>
