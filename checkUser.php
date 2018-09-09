ac8b2dbdaa2f2cd4190e415e22aa0b27ff667d7b
Dopesky <oboke69@gmail.com>
2018-09-09 23:56:34 +0300

    A file to check if a userName exists in the whole database

--- checkUser.php
+++ checkUser.php
@@ -1,5 +1,17 @@
 <?php
-$user=htmlspecialchars($POST["user"]);
+$return;
+if(!isset($user)){
+	$user=htmlspecialchars($_POST["user"]);
+}
 require "connection.php";
-$stmt=$connection->query("select * from citizen");
+$stmt=$connection->query("select userName from citizen_profile where userName='$user' union all select adminUserName from admin_profile where adminUserName='$user' union all select userName from politician_profile where userName='$user'");
+if(mysqli_num_rows($stmt)>0){
+	$return='true';
+	echo "true";
+	return;
+}else{
+	$return='false';
+	echo "false";
+	return;
+}
 ?>
