0e1dec73678f0d92659bc0b1465ff8b4789e4e46
Dopesky <oboke69@gmail.com>
2018-09-12 00:39:53 +0300

    Added new table to be checked

--- checkUser.php
+++ checkUser.php
@@ -3,8 +3,8 @@ $return;
 if(!isset($user)){
 	$user=htmlspecialchars($_POST["user"]);
 }
-require "connection.php";
-$stmt=$connection->query("select userName from citizen_profile where userName='$user' union all select adminUserName from admin_profile where adminUserName='$user' union all select userName from politician_profile where userName='$user'");
+require "Connection.php";
+$stmt=$connection->query("select userName from citizen_profile where userName='$user' union all select adminUserName from admin_profile where adminUserName='$user' union all select userName from politician_profile where userName='$user' union all select userName from deactivated_accounts where userName='$user'");
 if(mysqli_num_rows($stmt)>0){
 	$return='true';
 	echo "true";
