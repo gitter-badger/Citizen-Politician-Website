d0a86200874c0c29a22a144fdb9e299e813347b3
Dopesky <oboke69@gmail.com>
2018-09-21 01:27:02 +0300

    added lang attr to head element

--- Connection.php
+++ Connection.php
@@ -9,8 +9,8 @@
 
 
 <!DOCTYPE html>
-<html>
-<head lang="en">
+<html lang="en">
+<head>
 	<title>Mwananchi</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
