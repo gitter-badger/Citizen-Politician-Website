947c998836d09caa76b78d3600228c7c50e78fed
Dopesky <oboke69@gmail.com>
2018-09-21 03:18:35 +0300

    Started development for the official startpage for all users

---
+++ Stories.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+	header("Location: HomePage.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mwananchi</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" type="image/png" href="MwananchiIcon.png">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="Logged.css">
+	<link rel="stylesheet" type="text/css" href="LoggedPhone.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color: whitesmoke">
+	<div class="container-fluid">
+		<?php
+			require 'NavBar.php';
+		?>
+	</div>
+
+</body>
+</html>
