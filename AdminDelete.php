6689393ba20bb6d30e7316fc347397d17099ee16
Dopesky <oboke69@gmail.com>
2018-09-28 23:31:41 +0300

    Changed ajax call to form with post method for account de/re-activation. Added search functionality

--- AdminDelete.php
+++ AdminDelete.php
@@ -35,13 +35,32 @@ if(!isset($_SESSION["username"])||$_SESSION["usertype"]!=="admin"){
 	<div class="container" style="position:relative;top:100px">
 		<div class="alerts"></div>
 		<script>
-			var del=Cookies.get("delete")
+			var del=Cookies.get("success")
 			if(del!==undefined){
-				$('.alerts').addClass('alert').addClass('alert-info').html(del)
-				$("html, body").animate({ scrollTop: 0 }, "fast");
-				Cookies.remove("delete")
+				$('.alerts').addClass('alert').addClass('alert-success').html(del)
+				Cookies.remove("success")
 			}
 		</script>
+		<input class="form-control mb-3" type="text" placeholder="Search for specific user . . . " onkeyup="toggle(this)">
+			<script>
+				function toggle(event){
+					var value = $(event).val().toLowerCase();
+					var array= value.split(",")
+				    $('.all').filter(function() {
+				    	var count=0;
+				    	for(var i=0;i<array.length;i++){
+				    		if($(this).text().toLowerCase().indexOf(array[i])>-1){
+				    			count++
+				    		}
+				    	}
+				    	if(count!==array.length){
+				    		$(this).hide()
+				    	}else{
+				    		$(this).show()
+				    	}
+				    });
+				}
+			</script>
 		<div class="row d-flex justify-content-center">
 			<div class="col-lg-12"><div class="jumbotron"><span class="display-4 text-danger">Account Deactivation</span><br><small>The following are accounts registered on the system.</small></div></div>
 		</div>
@@ -58,7 +77,7 @@ if(!isset($_SESSION["username"])||$_SESSION["usertype"]!=="admin"){
 						if($count===0){
 							echo "<div class='row d-flex justify-content-center'><div class='col-lg-6 mb-3'><div class='row d-flex justify-content-center'>";
 						}
-						echo "<div class='col-sm-6 mb-3'><div class='card' style='width:250px'><img class='card-img-top' src='$row[3]' alt='Card image'><div class='card-body'><h4 class='card-title'>$row[0]</h4><p class='card-text'>$row[1]<br>$row[2]<br></p><a href='' class='btn btn-danger delete' id='$row[0]'>Deactivate Account</a></div></div></div>";
+						echo "<div class='col-sm-6 mb-3'><div class='card all' style='width:250px'><img class='card-img-top' src='$row[3]' alt='Card image'><div class='card-body'><h4 class='card-title'>$row[0]</h4><p class='card-text'>$row[1]<br>$row[2]<br></p><form action='Delete.php' method='post' class='delete' id='$row[0]'><input type='text' name='action' value='delete' style='display:none'><input style='display:none' type='text' name='user' value='$row[0]'><button type='submit' class='btn btn-danger'>Deactivate Account</button></form></div></div></div>";
 					}
 					echo "</div></div></div>";
 				}else{
@@ -84,7 +103,7 @@ if(!isset($_SESSION["username"])||$_SESSION["usertype"]!=="admin"){
 						if($count===0){
 							echo "<div class='row d-flex justify-content-center'><div class='col-lg-6 mb-3'><div class='row d-flex justify-content-center'>";
 						}
-						echo "<div class='col-sm-6 container mb-3'><div class='card' style='width:250px'><img class='card-img-top' src='$row[3]' alt='Card image'><div class='card-body'><h4 class='card-title'>$row[0]</h4><p class='card-text'>$row[1]<br>$row[2]<br></p><a href='' class='btn btn-success reactivate' id='$row[0]'>Reactivate Account</a></div></div></div>";
+						echo "<div class='col-sm-6 container mb-3'><div class='card all' style='width:250px'><img class='card-img-top' src='$row[3]' alt='Card image'><div class='card-body'><h4 class='card-title'>$row[0]</h4><p class='card-text'>$row[1]<br>$row[2]<br></p><form action='Delete.php' method='post' class='reactivate' id='$row[0]'><input type='text' name='action' value='reactivate' style='display:none'><input style='display:none' type='text' name='user' value='$row[0]'><button type='submit' class='btn btn-success'>Reactivate Account</button></form></div></div></div>";
 					}
 					echo "</div>";
 				}else{
@@ -96,25 +115,17 @@ if(!isset($_SESSION["username"])||$_SESSION["usertype"]!=="admin"){
 			?>
 	</div>
 	<script>
-		$(".delete").click(event=>{
-			event.preventDefault();
+		$("form.delete").submit(event=>{
 			var ans=confirm("Are you sure you want to deactivate account where username is "+$(event.currentTarget).attr("id")+"?");
-			if(ans){
-				$.post("Delete.php",{action:"delete",user:$(event.currentTarget).attr("id")},data=>{
-					Cookies.set("delete",data)
-					location.reload(true)
-				})
+			if(!ans){
+				return false;
 			}
 		})
 
-		$(".reactivate").click(event=>{
-			event.preventDefault();
+		$("form.reactivate").submit(event=>{
 			var ans=confirm("Are you sure you want to reactivate account where username is "+$(event.currentTarget).attr("id")+"?");
-			if(ans){
-				$.post("Delete.php",{action:"reactivate",user:$(event.currentTarget).attr("id")},data=>{
-					Cookies.set("delete",data)
-					location.reload(true)
-				})
+			if(!ans){
+				return false;
 			}
 		})
 	</script>
--- Delete.php
+++ Delete.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Mwananchi</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" type="image/png" href="MwananchiIcon.png">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:lavender">
 <?php
 	$action=htmlspecialchars($_POST["action"]);
 	$user=htmlspecialchars($_POST["user"]);
@@ -5,16 +24,25 @@
 	if($action==="delete"){
 		$sql = "insert into deactivated_accounts(userName,email,emailVerified,phone,phoneVerified,gender,accountType,countyNo,photo,Password) select * from citizen_profile where UserName='$user';insert into deactivated_accounts(userName,email,emailVerified,phone,phoneVerified,gender,accountType,countyNo,photo,Password) select userName,email,emailVerified,phone,phoneVerified,gender,accountType,countyNo,photo,password from politician_profile where userName='$user';insert into deactivated_politics(userName,FullNames,DateOfBirth,PoliticalSeat,PoliticalYears,CreationTime,Vying,ConstituencyNo,WardNo) select * from politician_politics where userName='$user';insert into deactivated_education(userName,bachelors,primarySchool,secondarySchool,university,masters,phd,schoolCertificates,display,mastersCourse,phdCourse,otherCourses) select * from politician_education where userName='$user'; delete from citizen_profile where userName='$user';delete from politician_profile where userName='$user'";
 	 	if(mysqli_multi_query($connection,$sql)){
-	 		echo "Successfully deactivated user account with username $user.";
-	 	}else{
-	 		echo "Failed to deactivated user account with username $user. $connection->error";
+	 		echo "<script>Cookies.set('success','Successfully deactivated user account with username $user.');location.replace('AdminDelete.php')</script>";
 	 	}
 	}else if($action==="reactivate"){
 		$sql = "insert into citizen_profile(UserName,Email,verifyEmail,phone,verifyPhone,gender,type,County,photo,Secret) select * from deactivated_accounts where userName='$user' and accountType='citizen';insert into politician_profile(userName,email,emailVerified,phone,phoneVerified,gender,accountType,countyNo,photo,password) select * from deactivated_accounts where userName='$user' and accountType='politician';insert into politician_politics(userName,FullNames,DateOfBirth,PoliticalSeat,PoliticalYears,CreationDate,Vying,ConstituencyNo,WardNo) select * from deactivated_politics where userName='$user';insert into politician_education(userName,bachelors,primarySchool,secondarySchool,university,masters,phd,schoolCertificates,display,mastersCourse,phdCourse,otherCourses) select * from deactivated_education where userName='$user';update politician_profile set accountVerified=1 where userName='$user';delete from deactivated_accounts where userName='$user';delete from deactivated_politics where userName='$user';delete from deactivated_education where userName='$user';";
 		if(mysqli_multi_query($connection,$sql)){
-	 		echo "Successfully reactivated user account with username $user.";
-	 	}else{
-	 		echo "Failed to reactivated user account with username $user. $connection->error";
+	 		echo "<script>Cookies.set('success','Successfully reactivated user account with username $user.');location.replace('AdminDelete.php')</script>";
 	 	}
  	}
 ?>
+<br><br>
+	<div class="container alert alert-danger">
+		<h1 class="jumbotron text-secondary"><i class="fas fa-exclamation-triangle"></i> Error Deactivating or Reactivating Account. <i class="fas fa-exclamation-triangle"></i></h1>
+		<div class="alert alert-info">Please take a screenshot and contact administrator of: <i class="text-secondary">https://mwananchi.herokuapp.com</i><br><strong>DO NOT TRY TO FIX ON YOUR OWN!</strong><br>We are sorry for any inconveniences this might have caused to you.</div>
+		<div class="alert alert-danger"><i>
+			<?php
+				echo ($connection) ? "" : $connection->error;
+				echo ($stmt) ? "" : $stmt->error;
+			?>
+		</i></div>
+	</div>
+</body>
+</html>
