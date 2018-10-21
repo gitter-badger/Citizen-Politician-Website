16ee54db704c8a08c2b32a969b674b19d7a5bfd1
Dopesky <oboke69@gmail.com>
2018-10-22 01:41:11 +0300

    Linked controller to model

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -3,8 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profile extends CI_Controller {
 	public function index($user){
-		$data['head']=$this->loadscripts->index().$this->loadscripts->load_datatable();
+		$this->load->model("profile_model");
 		$data['navbar']=$this->navbar->load_nav_bar();
+		$data['top']=$this->profile_model->index($user);
+		$data['head']=$this->loadscripts->index().$this->loadscripts->load_datatable();
 		$this->load->view("profile_viewer",$data);
 	}
 }
