c624ac13fbb27d9aa4577d847510e7018c56c7b9
Dopesky <oboke69@gmail.com>
2018-11-01 16:10:52 +0300

    Added the deafult database

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['test'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'biggie5941',
 	'database' => 'citizen-politician website',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'sql2.freemysqlhosting.net',
+	'username' => 'sql2263816',
+	'password' => 'sB7%pI1*',
+	'database' => 'sql2263816',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
