5d058c8a35c3cec13ebe04cc5e7ec8389a220220
Dopesky <oboke69@gmail.com>
2018-11-16 03:36:49 +0300

    Changed the about document displayed on index page

--- application/views/index.php
+++ application/views/index.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<div class="alert alert-info mb-1" id="about" style="position: relative;overflow: auto;display: inline-block;opacity: 0;">
 					<h1 class="jumbotron" style="padding-bottom:5px;padding-top: 10px;font-family: Cookie,cursive;font-size: 54px;"><i class="fas fa-user"></i> Mwananchi Website<br><pre style="font-size: 24px;font-family: Cookie,cursive;color: darkslategray;">            Leadership with service.</pre></h1>
 					Welcome to Mwananchi Website. The website is designed for Kenyan citizens to enable them to <strong>know their politicians</strong>. Kenyans do not usually know the type of leader they select. In most cases, citizens often vote for people in their party or their tribe. After elections, the elected leaders do not fulfil their duties as stipulated in the constitution or their manifesto. Citizens have no way of contacting their leaders to raise a complaint or observation. <br>This web application has come to the rescue. It <strong>displays information about a politician</strong> that is relevant to the public. With this application citizens can also <strong>contact their leaders</strong> and also <strong>post problems and comments related to their respective areas</strong>. Citizens will also be able to see <strong>a detailed analysis of their leaders to help them decide who to vote for.</strong><br>
-					The developers <i>Kevin Mwenda</i> and <i>Esther Njoroge</i> believe that this web application is the key to help Kenyans move foward politically.
+					The developer <i>Kevin Mwenda</i> believes that this web application is the key to help Kenyans move foward politically.
 				</div>
 			</td>
 			<td style="width: 50%;padding-left: 25px;padding-top: 190px">
@@ -38,7 +38,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</div>	
 					<div id="one" style="position: relative;left:0;overflow: auto;width: 100%;height: 100%;">
 						<div id="two" style="bottom:0;">
-							<div style="position: relative;color: gray;margin-left:-350px;font-family: Cookie,cursive;font-size: 24px;" id="three">Created by: Kevin and Esther...</div>
+							<div style="position: relative;color: gray;margin-left:-350px;font-family: Cookie,cursive;font-size: 24px;" id="three">Created by: Kevin...</div>
 							<div id="four" style="position: relative;color: gray;font-family: Cookie,cursive;font-size: 24px;">Version: <span id="version" style="font-size: 22px;letter-spacing: 2px;color: whitesmoke;transition: color 1.5s"></span><br>Year: <span id="year" style="font-size: 22px;letter-spacing: 2px;color: whitesmoke;transition: color 1.5s"></span></div><br><br>
 							<button class="btn btn-info" style="color: lightgray;position: relative;font-family: Cookie,cursive;font-size: 24px;padding: 5px 10px;transition: all 0.5s;transition-timing-function: ease-out;display: inline-block;text-align: center;width: 90px;opacity: 0;margin-left: 100px;" onclick="location.assign('<?php echo base_url()?>home.html')"><span>Proceed</span></button>
 						</div>
