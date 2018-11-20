38311802c560f48efbaf4853250d51bc883cf7c4
Dopesky <oboke69@gmail.com>
2018-11-20 19:16:52 +0300

    Added methods to redirect to business pages

--- application/controllers/Business.php
+++ application/controllers/Business.php
@@ -7,5 +7,37 @@ class Business extends CI_Controller {
 		parent::__construct();
 		$this->load->model('regions');
 	}
+
+	public function privacy(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function terms(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function help(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function cookies(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function security(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function features(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function dev_zone(){
+		redirect(site_url("coming_soon"),'location');
+	}
+
+	public function about(){
+		redirect(site_url("coming_soon"),'location');
+	}
 }
 ?>
