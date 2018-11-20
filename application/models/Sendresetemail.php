6bc3117cd6aa8abcb8b75c7347b47812f06614e3
Dopesky <oboke69@gmail.com>
2018-11-20 19:24:52 +0300

    Changed the random generate string function to Formatter class and changed email subject

--- application/models/Sendresetemail.php
+++ application/models/Sendresetemail.php
@@ -18,10 +18,10 @@ class Sendresetemail extends CI_Model {
 			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email because your email is not verified.</div>";
 		}
 		if($this->log_event($email->user)){
-			if($this->email->send_email($email->email,"Password Reset Request.","We hope that you are well. Below is a link that will enable you to reset your password. <br><br> <a style='text-align:center;border-radius:5px;background-color: #03869b;color: white;padding:10px;text-decoration:none;' href='".site_url("reset_user_password/$email->user/$this->passcode")."'>Reset My Password</a><br>",$email->user)){
+			if($this->email->send_email($email->email,"Password Reset Instructions.","We hope that you are well. Below is a link that will enable you to reset your password. <br><br> <a style='text-align:center;border-radius:5px;background-color: #03869b;color: white;padding:10px;text-decoration:none;' href='".site_url("reset_user_password/$email->user/$this->passcode")."'>Reset My Password</a><br>",$email->user)){
 				return "<div class='alert alert-success'><strong>Success!</strong> Password Reset Email Successfully Sent to your Email.</div>";
 			}else{
-				return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email. ".$this->emailprepare->mailer->ErrorInfo."</div>";
+				return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email. ".$this->email->mailer->ErrorInfo."</div>";
 			}
 		}else{
 			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email because a Database Error Occurred. Please Contact Administrator.</div>";
@@ -31,19 +31,9 @@ class Sendresetemail extends CI_Model {
 	private function log_event($email){
 		if($this->db->query("update emailgetcredentials set used=1 where userEmail=? and type='password'",$email)){
 			$newID=$this->db->query("select max(eventID) as maxID from emailgetcredentials")->row()->maxID+1;
-			return $this->db->query("insert into emailgetcredentials(eventID,userEmail,passCode,type) values (?,?,?,'password')",array($newID,$email,$this->passcode=$this->generate_random_string(random_int(10,20))));
+			return $this->db->query("insert into emailgetcredentials(eventID,userEmail,passCode,type) values (?,?,?,'password')",array($newID,$email,$this->passcode=$this->formatter->generate_random_string(random_int(10,20))));
 		}
 		return false;
-	}
-
-	public function generate_random_string($integer) {
-	  $text = "";
-	  $possible_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-	  for ($i = 0; $i < $integer; $i++)
-	    $text .= $possible_chars[random_int(0, strlen($possible_chars) - 1)];
-
-	  return $text;
 	}
 
 	public function get_log($passcode){
