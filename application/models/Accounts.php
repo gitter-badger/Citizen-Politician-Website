ebd3baf97f8a61260616a407234cd8adc91d0457
Dopesky <oboke69@gmail.com>
2018-11-30 19:54:54 +0300

    Added comments, removed unnecessary functions and optimized existing ones

--- application/models/Accounts.php
+++ application/models/Accounts.php
@@ -2,58 +2,68 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Accounts extends CI_Model {
+	//constructor
 	function __construct(){
 		parent::__construct();
 	}
 
+	//function for logging in an admin by email or username.
 	public function login_admin($user_name){
-		return $this->db->query("select adminUserName as username,adminPassword as pass,photo,userType as usertype from admin_profile where adminUserName=?",$user_name)->row();
+		return $this->db->query("select adminUserName as username,adminPassword as pass,photo,userType as usertype from admin_profile where adminUserName=? or adminEmail=?",array($user_name,$user_name))->row();
 	}
 
+
+	//function for logging in a politician by email, password or phone number. NB: Politicians whose accounts are not verified by admin cannot use their accounts. Politicians who have not verified their emails and phones can also not use their accounts.
 	public function login_politician($user_name){
-		return (is_object($this->login_politician_email($user_name))) ? $this->login_politician_email($user_name) : $this->login_politician_user($user_name);
+		return $this->db->query("select userName as username,Password as pass,photo,accountType as usertype from politician_profile where (userName=? or email=? or phone=?) and accountVerified=1 and emailVerified=1 and phoneVerified=1",array($user_name,$user_name,$user_name))->row();
 	}
 
-	public function check_politician($user_name){
-		return $this->db->query("select * from politician_profile where userName=? and countyNo=? and accountVerified=1",array($user_name,$this->session->userdata('county')))->row();
-	}
-
-	private function login_politician_email($email){
-		return $this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where email=? and accountVerified=1",$email)->row();
-	}
-
-	private function login_politician_user($user_name){
-		return $this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where userName=? and accountVerified=1",$user_name)->row();
-	}
-
+	//function for logging in a citizen by email, username or phone number. For phone numbers, the accepted format is the one saved on the database ie country code+number with the + sign. eg +254789123456 for Kenya where +254 is country code and 789123456 is the phone number
 	public function login_citizen($user_name){
-		return (is_object($this->login_citizen_email($user_name))) ? $this->login_citizen_email($user_name) : $this->login_citizen_user($user_name);
+		return $this->db->query("select userName as username,Secret as pass,photo,type as usertype from citizen_profile where UserName=? or Email=? or phone=?",array($user_name,$user_name,$user_name))->row();
 	}
 
-	private function login_citizen_email($email){
-		return $this->db->query("select UserName as username,Secret as pass,photo,type as usertype,County as county from citizen_profile where Email=?",$email)->row();
+	//function to check if a politician is available to use as a target in posts. This function only checks for username meaning the target for any post can only be a politician's username and not their real name or any other information relating to them.
+	public function check_politician($user_name){
+		return $this->db->query("select * from politician_profile where userName=? and accountVerified=1",$user_name)->row();
 	}
 
-	private function login_citizen_user($user_name){
-		return $this->db->query("select userName as username,Secret as pass,photo,type as usertype,County as county from citizen_profile where UserName=?",$user_name)->row();
-	}
 
+	//since email is a unique field in the database, this function gets the email,username and phone number for any user in the system. One can pass an email, username or phone number as a parameter to this function. Thus it can be used to check if the email, username or phone number exists in the database. It can also be used to get the other parameters given one of them.
 	public function get_email($username){
-		$posAt=strpos($username, "@");
-		$posDot=strripos($username, ".");
-		return ($posAt<1||($posAt+2)>$posDot||($posDot+2)>=strlen($username)) ? $this->db->query("select UserName as user,Email as email from citizen_profile where UserName=? union select userName as user,email from politician_profile where userName=?",array($username,$username))->row():$this->db->query("select UserName as user,Email as email from citizen_profile where Email=? union select userName as user,email from politician_profile where email=?",array($username,$username))->row();
+		return $this->db->query("select UserName as user,Email as email,phone from citizen_profile where UserName=? or Email=? or phone=? union all select userName as user,email,phone from politician_profile where userName=? or email=? or phone=? union all select adminUserName as user,adminEmail as email,null as phone from admin_profile where adminUserName=? or adminEmail=?",array($username,$username,$username,$username,$username,$username,$username,$username))->row();
 	}
 
+	//This function checks a particular EMAIL to see if it is verified. Verified emails can be used to reset passwords. If an email is not verified one cannot use it in the password reset functionality.
 	public function check_email_verified($email){
-		return $this->db->query("select emailVerified as verified from politician_profile where email=? union all select verifyEmail as verified from citizen_profile where Email=?",array($email,$email))->row()->verified;
+		return $this->db->query("select emailVerified as verified from politician_profile where email=? union all select verifyEmail as verified from citizen_profile where Email=? union all select verified from admin_profile where adminEmail=?",array($email,$email,$email))->row()->verified;
 	}
 
+	//This function checks a particular PHONE NUMBER to see if it is verified. Verified phone numbers can be used to reset passwords. If a phone number is not verified one cannot use it in the password reset functionality.
+	public function check_phone_verified($phone){
+		return $this->db->query("select phoneVerified as verified from politician_profile where phone=? union all select verifyPhone as verified from citizen_profile where phone=? union all select 0 as verified from admin_profile",array($phone,$phone))->row()->verified;
+	}
+
+	//This function needs work
 	public function get_politician_info($politician){
 		return $this->db->query("select * from politician_profile left join politician_politics on politician_profile.userName=politician_politics.userName left join politician_education on politician_profile.userName=politician_education.userName left join counties on CountyID=countyNo left join constituencies on constituencyID=ConstituencyNo left join wards on wardID=WardNo where politician_profile.userName=?",$politician)->row();
 	}
 
-	public function check_verified($politician){
-		return ($this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where email=?",$politician)->row()===null)?$this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where userName=?",$politician)->row():$this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where email=?",$politician)->row();
+	//Incase a politician is denied access to the site on login, this function checks why they were denied access ie what credentials were missing eg email was not verified or account is non existent.
+	public function check_login_error($politician){
+		$error=$this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where (userName=? or email=?) and emailVerified=0",$politician,$politician)->row();
+		if(!isset($error)){
+			$error=$this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where (userName=? or email=?) and phoneVerified=0",$politician,$politician)->row();
+			if(!isset($error)){
+				$error=$this->db->query("select userName as username,Password as pass,photo,accountType as usertype,countyNo as county from politician_profile where (userName=? or email=?) and accountVerified=0",$politician,$politician)->row();
+				if(!isset($error)){
+					return "not found";
+				}
+				return "account";
+			}
+			return "phone";
+		}
+		return "email";
 	}
 
 	public function check_name($username){
