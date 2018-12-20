224d23ca56374a01c5bd20b676a9befc0c367be3
Dopesky <oboke69@gmail.com>
2018-12-20 17:50:12 +0300

    Added comments to class

--- application/models/Regions.php
+++ application/models/Regions.php
@@ -6,10 +6,12 @@ class Regions extends CI_Model {
 		parent::__construct();
 	}
 
+	//get all registred counties from the db
 	public function get_counties(){
 		return $this->db->query("select CountyID,County,countryNo from counties")->result();
 	}
 
+	//get all registered countries from the db
 	public function get_countries(){
 		return $this->db->query("select countryID,country from countries")->result();
 	}
