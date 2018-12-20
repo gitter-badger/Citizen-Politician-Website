b7b6f6a6b29e1ff54eb19fcf4c71e7b29dbcbd3d
Dopesky <oboke69@gmail.com>
2018-12-20 17:49:06 +0300

    added functions to send a password reset text

--- application/models/Sendresetphone.php
+++ application/models/Sendresetphone.php
@@ -1,9 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sendresetphone extends Sendresetemail {
+class Sendresetphone extends CI_Model {
+	private $passcode;
 	public function __construct(){
 		parent::__construct();
+		$this->load->library('phone');
+		$this->load->model('accounts');
 	}
 
 	public function index($email){
@@ -15,18 +18,25 @@ class Sendresetphone extends Sendresetemail {
 			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Phone because your phone number is not on the system.</div>";
 		}
 		if($this->accounts->check_phone_verified($email->phone)==0){
-			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Phone because your phone is not verified.</div>";
+			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Phone because your phone number is not verified.</div>";
 		}
 		if($this->log_event($email->user)){
-			$phone='0'.strrev(substr(strrev($email->phone),0,9));
-			if($this->email->send_email($phone.'@vtext.com',"Password Reset Instructions.","We hope that you are well. Below is a link that will enable you to reset your password. <br><br> <a style='text-align:center;border-radius:5px;background-color: #03869b;color: white;padding:10px;text-decoration:none;' href='".site_url("reset_user_password/$email->user/$this->passcode")."'>Reset My Password</a><br>",$email->user)){
-				return "<div class='alert alert-success'><strong>Success!</strong> Password Reset Email Successfully Sent to your Email.</div>";
+			if(!empty($this->phone->send_text("Your Password Reset code is $this->passcode",$email->phone))){
+				return true;
 			}else{
-				return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email. ".$this->email->mailer->ErrorInfo."</div>";
+				return "<div class='alert alert-danger'><strong>Fail!</strong> An error occurred and a text could not be sent. Try using email to reset password.</div>";
 			}
 		}else{
-			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Email because a Database Error Occurred. Please Contact Administrator.</div>";
+			return "<div class='alert alert-danger'><strong>Fail!</strong> Password Reset Instructions NOT Sent to your Phone because a Database Error Occurred. Please Contact Administrator.</div>";
 		}
+	}
+
+	//logs the event in the database for future reference i.e during actual password reset.
+	private function log_event($email){
+		if($this->db->query("update emailgetcredentials set used=1 where userEmail=? and type='password'",$email)){
+			return $this->db->query("insert into emailgetcredentials(userEmail,passCode,type) values (?,?,'password')",array($email,$this->passcode=$this->formatter->generate_random_code(random_int(4,6))));
+		}
+		return false;
 	}
 }
 ?>
