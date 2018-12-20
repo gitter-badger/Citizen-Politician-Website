1009ccf59a37b7c2ad6f6265778dfb024b3572ae
Dopesky <oboke69@gmail.com>
2018-12-20 11:38:27 +0300

    Changed everything to functions

--- resources/js/main_js.js
+++ resources/js/main_js.js
@@ -10,79 +10,68 @@
 		$(input).focus()
 	}
 
-	$("div#signUp>form").submit(event=>{
-		var user=$("#user").val().trim()
-		var email=$("#email").val()
-		var phone=$("#phone").val()
-		var pass=$("#secret").val()
-		var passRe=$("#secretRe").val()
-		var image=$("#photo").val()
-		var county=$("#counties").find(":selected").attr("id")
-		var acc=$("input[name=type]:checked").val()
-		$("#userError,#emailError,#phoneError,#secretError,#secretReError").removeClass("alert").removeClass("alert-danger").html("")
-		
+	function sign_up_user(event){
+		var user=$(event).find("input[name='user']").val().trim()
+		var email=$(event).find("input[name='email']").val().trim()
+		var phone=$(event).find("input[name='phone']").val().trim()
+		var pass=$(event).find("input[name='secret']").val()
+		var passRe=$(event).find("input[name='secretRe']").val()
+		$("div#sign_up_error").html("")
 
+		//check if data is available
+		if(user.length<1||email.length<1||phone.length<1||pass.length<1||passRe.length<1){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Please fill out all fields.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
+
+		//verify username format
 		if(user.length<3){
-			$("div#userError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Username must be atleast 3 characters.")
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Username must be atleast 3 characters.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
 			return false;
 		}
 		if(!isNaN(user.charAt(0))){
-			$("div#userError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Username cannot start with a number.")
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Username cannot start with a number.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
 			return false;
 		}
-		$("div#userError").removeClass("alert").removeClass("alert-danger").html("")
+		if(user.toLowerCase().localeCompare('mwananchi')===0){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>You cannot use the name 'mwananchi' as a usernname.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
+		if((/[^a-z 0-9-_.]/i).test(user)){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Username has unwanted symbols.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
 
-		if(pass.length<8){
-			$("div#secretError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Passwords should be 8 characters long.")
+		//verify email format
+		if(email.indexOf("@")<1||(email.indexOf("@")+2)>email.lastIndexOf(".")||(email.lastIndexOf(".")+2)>=email.length){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Email is of invalid format.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
 			return false;
 		}
-		for(var i=0;i<pass.length;i++){
-			if(isNaN(pass.charAt(i))){
-				if(i===pass.length-1){
-					$("div#secretError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Passwords should be have atleast 1 number.")
-					return false;
-				}
-			}else
-				break;
-		}
-		for(var i=0;i<pass.length;i++){
-			if(isNaN(pass.charAt(i))){
-				break
-			}else{
-				if(i===pass.length-1){
-					$("div#secretError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Passwords should have atleast 1 letter.")
-					return false;
-				}
-			}
-		}
-		$("div#secretError").removeClass("alert").removeClass("alert-danger").html("")
 
-		if(passRe.localeCompare(pass)!==0){
-			$("div#secretReError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Passwords should match.")
-			return false;
-		}
-		$("div#secretReError").removeClass("alert").removeClass("alert-danger").html("")
-
-		if(email.trim().indexOf("@")<1||(email.trim().indexOf("@")+2)>email.trim().lastIndexOf(".")||(email.trim().lastIndexOf(".")+2)>=email.trim().length){
-			$("div#emailError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Email is not a valid email.")
-			return false;
-		}
-		$("div#emailError").removeClass("alert").removeClass("alert-danger").html("")
-
-		if(phone.trim().charAt(0)!=7){
-			$("div#phoneError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Number must start with 7.")
-			return false;
-		}
-		if(phone.trim().length!=9){
-			$("div#phoneError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Number must be exactly 9 characters long.")
-			return false;
-		}
+		//verify phone number format
 		if(isNaN(phone)){
-			$("div#phoneError").addClass("alert").addClass("alert-danger").html("<strong>Error: </strong>Number must be a number.")
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Please use a valid phone number.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
 			return false;
 		}
-		$("div#phoneError").removeClass("alert").removeClass("alert-danger").html("")
-	})
+
+		//verify password format
+		if(pass.length<8){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Passwords should be 8 characters long.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
+		if(!(/[a-z]/i).test(pass)||!(/[0-9]/i).test(pass)){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Passwords should contain atleast 1 character and 1 number.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
+
+		//check that passwords match
+		if(passRe.localeCompare(pass)!==0){
+			$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>Passwords should match.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+			return false;
+		}
+
+		return true;
+	}
 
 	function checkUser(input,span){
 		var user=$(input).val().trim()
@@ -160,24 +149,9 @@
 			$(span).removeClass("fa-check").addClass("fa-times").css("color","indianred")
 			return;
 		}
-		for(var i=0;i<secret.length;i++){
-			if(isNaN(secret.charAt(i))){
-				if(i===secret.length-1){
-					$(span).removeClass("fa-check").addClass("fa-times").css("color","indianred")
-					return
-				}
-			}else
-				break;
-		}
-		for(var i=0;i<secret.length;i++){
-			if(isNaN(secret.charAt(i))){
-				break
-			}else{
-				if(i===secret.length-1){
-					$(span).removeClass("fa-check").addClass("fa-times").css("color","indianred")
-					return
-				}
-			}
+		if(!(/[a-z]/i).test(secret)||!(/[0-9]/).test(secret)){
+			$(span).removeClass("fa-check").addClass("fa-times").css("color","indianred")
+			return
 		}
 		$(span).removeClass("fa-times").addClass("fa-check").css("color","limegreen")
 	}
@@ -196,7 +170,7 @@
 		$(span).removeClass("fa-times").addClass("fa-check").css("color","limegreen")
 	}
 
-	function check_photo(event,label){
+	function check_photo(event,label,error){
 		var photo=$(event.currentTarget).val()
 		if(photo.length===0){
 			$(label).html("Profile Photo <span class='text-secondary'>(Optional): </span>")
@@ -204,7 +178,7 @@
 		}
 		var ext=photo.substr(photo.lastIndexOf(".")+1)
 		if(ext.toLowerCase().localeCompare("png")!==0 && ext.toLowerCase().localeCompare("jpg")!==0 && ext.toLowerCase().localeCompare("jpeg")!==0 && ext.toLowerCase().localeCompare("gif")!==0){
-			alert("Please select valid image file i.e gif,png or jpeg.")
+			$(error).html("<div class='alert alert-danger alert-dismissable fade show'>Please use png, jpeg or gif image formats.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
 			$(event.currentTarget).val("")
 			$(label).html("Profile Photo <span class='text-secondary'>(Optional): </span>")
 			return;
@@ -246,10 +220,12 @@
 		return diff;
 	}
 
-	function change_page($event,$scope,$sce,loader,url,top){
+	function change_page($event,$scope,$sce,assets,url){
 		if($($event.currentTarget).text()==$scope.current_page||isNaN(parseInt($($event.currentTarget).text()))) return
 		$scope.data=[]
-		$(loader).css({display:'table-row'})
+		$(assets.loader).css({display:'table-row'})
+		$(assets.pagination).css({display:'none'})
+		$(assets.search_element).closest('form').css({display:'none'})
 		var offset=$($event.currentTarget).attr('data-offset')
 		$.post(url,{search:$scope.search_field,offset:offset},data=>{
 			$scope.$apply(()=>{
@@ -260,9 +236,11 @@
 				$scope.current_page=parseInt($($event.currentTarget).text())
 				update_page_list($scope)
 			})
+			$(assets.pagination).css({display:'table-row'})
 			$scope.check_page()
-			$('body,html').scrollTop($(top).offset().top)
-			$(loader).css({display:'none'})
+			$('body,html').scrollTop($(assets.top).offset().top)
+			$(assets.loader).css({display:'none'})
+			$(assets.search_element).closest('form').css({display:'initial'})
 		})
 	}
 
@@ -330,10 +308,40 @@
 		$scope.pages_array[5]=['...',10*($scope.current_page+1),($scope.current_page+2)+"-"+($scope.page_count-1)] 
 	}
 
-	function search_angular($scope,$sce,loader,url){
+	function search_angular($scope,$sce,assets,url){
 		$scope.data=[]
 		$scope.pages_array=[]
-		$(loader).css({display:'table-row'})
+		$(assets.loader).css({display:'table-row'})
+		$(assets.pagination).css({display:'none'})
+		$(assets.search_element).closest('form').css({display:'none'})
+		$(assets.no_data).css({display:'none'})
+		if($scope.current_page===-1){
+			if(sessionStorage.getItem('data')!==undefined){
+				if(new Date().getTime()-sessionStorage.getItem('faq_time')<180000){
+					data=JSON.parse(sessionStorage.getItem('faq_data'))
+					for(each in data[0]){
+						$scope.data.push([data[0][each].contactID,data[0][each].name,data[0][each].email,$sce.trustAsHtml(data[0][each].question),$sce.trustAsHtml(data[0][each].reply),diff_date(data[0][each].time),post_date(data[0][each].time)])
+					}
+					$scope.data_count=data[1]
+					$scope.page_count=Math.ceil($scope.data_count/10)
+					for(var i=0;i<$scope.page_count;i++){
+						if(i===5&&$scope.page_count>7){
+							$scope.pages_array.push(['...',10*i,'6-'+($scope.page_count-1)])
+							$scope.pages_array.push([$scope.page_count,10*($scope.page_count-1),$scope.page_count])
+							break
+						}
+						$scope.pages_array.push([i+1,10*i,i+1])
+					}
+					$scope.current_page=1
+					$(assets.pagination).css({display:'table-row'})
+					$scope.check_page()
+					$(assets.loader).css({display:'none'})
+					$(assets.search_element).closest('form').css({display:'initial'})
+					$scope.search_field=sessionStorage.getItem('faq_search_string')
+					return;
+				}
+			}
+		}
 		$.post(url,{search:$scope.search_field,offset:0},data=>{
 			$scope.$apply(()=>{
 				data=JSON.parse(data)
@@ -351,43 +359,50 @@
 					$scope.pages_array.push([i+1,10*i,i+1])
 				}
 				$scope.current_page=1
+				sessionStorage.setItem('faq_data',JSON.stringify(data))
+				sessionStorage.setItem('faq_time',new Date().getTime())
+				sessionStorage.setItem('faq_search_string',$scope.search_field)
 			})
+			$(assets.pagination).css({display:'table-row'})
 			$scope.check_page()
-			$(loader).css({display:'none'})
+			$(assets.loader).css({display:'none'})
+			$(assets.search_element).closest('form').css({display:'initial'})
 		})
 	}
 
-	function check_page($scope,pagination,no_data,previous,next){
+	function check_page($scope,assets){
 		if($scope.page_count==0){
-			$(pagination).css({display:'none'})
-			$(no_data).css({display:'table-row'})
+			$(assets.pagination).css({display:'none'})
+			$(assets.no_data).css({display:'table-row'})
 			return
 		}else{
-			$(pagination).css({display:'table-row'})
-			$(no_data).css({display:'none'})
+			$(assets.pagination).css({display:'table-row'})
+			$(assets.no_data).css({display:'none'})
 		}
 		if($scope.current_page==1){
-			$(previous).parent().addClass('disabled')
+			$(assets.previous).parent().addClass('disabled')
 			if($scope.current_page!==$scope.page_count){
-				$(next).parent().removeClass('disabled')
+				$(assets.next).parent().removeClass('disabled')
 			}
 		}
 		if($scope.current_page==$scope.page_count){
-			$(next).parent().addClass('disabled')
+			$(assets.next).parent().addClass('disabled')
 			if($scope.current_page!==1){
-				$(previous).parent().removeClass('disabled')
+				$(assets.previous).parent().removeClass('disabled')
 			}
 		}
 		if($scope.current_page>1&&$scope.current_page<$scope.page_count){
-			$(previous).parent().removeClass('disabled')
-			$(next).parent().removeClass('disabled')
+			$(assets.previous).parent().removeClass('disabled')
+			$(assets.next).parent().removeClass('disabled')
 		}
 	}
 
-	function prev_next($event,$scope,$sce,loader,url,top){
+	function prev_next($event,$scope,$sce,assets,url){
 		if(($($event.currentTarget).attr('title').localeCompare('Previous')===0 && $scope.current_page===1)||($($event.currentTarget).attr('title').localeCompare('Next')===0 && $scope.current_page===$scope.page_count)) return
 		$scope.data=[]
-		$(loader).css({display:'table-row'})
+		$(assets.loader).css({display:'table-row'})
+		$(assets.pagination).css({display:'none'})
+		$(assets.search_element).closest('form').css({display:'none'})
 		var offset=($($event.currentTarget).attr('title').localeCompare('Previous')===0) ? 10*($scope.current_page-2):10*($scope.current_page)
 		$.post(url,{search:$scope.search_field,offset:offset},data=>{
 			$scope.$apply(()=>{
@@ -398,9 +413,11 @@
 				$scope.current_page=($($event.currentTarget).attr('title').localeCompare('Previous')===0) ? $scope.current_page-1:$scope.current_page+1
 				update_page_list($scope)
 			})
+			$(assets.pagination).css({display:'table-row'})
 			$scope.check_page()
-			$('body,html').scrollTop($(top).offset().top)
-			$(loader).css({display:'none'})
+			$('body,html').scrollTop($(assets.top).offset().top)
+			$(assets.loader).css({display:'none'})
+			$(assets.search_element).closest('form').css({display:'initial'})
 		})
 	}
 
@@ -412,25 +429,92 @@
   		}
 	}
 
-	function fade(){
-		if(screen.width>992){
-			if($(window).width()>992){
-				var div=$("#main").height()
-				var top=$("body,html").scrollTop()
-				var percentage=1-(parseFloat(top)/parseFloat(div))
-				if(percentage>0.1){
-					$("#intro,#loginForm").fadeTo(0,percentage)
+	function dateChooser($scope,$window){
+		var allMonths=['January','February','March','April','May','June','July','August','September','October','November','December']
+		$scope.days=[]
+		$scope.months=[]
+		$scope.years=[]
+		for(var i=1990;i<=new Date().getFullYear();i++){
+			$scope.years.push(i)
+		}
+		$scope.get_months=function(){
+			$scope.months=[]
+			if($scope.year==new Date().getFullYear().toString()){
+				for(var i=0;i<(new Date().getMonth()+1);i++){
+					$scope.months.push([(i+1),allMonths[i]])
+				}
+				if(parseInt($scope.month)>(new Date().getMonth()+1)){
+					$scope.month='1'
 				}
 			}else{
-				$("#intro,#loginForm").fadeTo(0,1)
+				for(var i=0;i<12;i++){
+					$scope.months.push([(i+1),allMonths[i]])
+				}
+			}
+			$scope.get_days()
+		}
+		$scope.get_days=function(){
+			$scope.days=[]
+			if($scope.month==(new Date().getMonth()+1).toString()&&$scope.year==new Date().getFullYear().toString()){
+				for(var i=0;i<new Date().getDate();i++){
+					$scope.days.push(i+1)
+				}
+			}else{
+				if($scope.month==2){
+					if(parseInt($scope.year)%4===0){
+						for(var i=0;i<29;i++){
+							$scope.days.push(i+1)
+						}
+					}else{
+						for(var i=0;i<28;i++){
+							$scope.days.push(i+1)
+						}
+					}
+				}else if(parseInt($scope.month)<=7){
+					if(parseInt($scope.month)%2===0){
+						for(var i=0;i<30;i++){
+							$scope.days.push(i+1)
+						}
+					}else{
+						for(var i=0;i<31;i++){
+							$scope.days.push(i+1)
+						}
+					}
+				}else{
+					if(parseInt($scope.month)%2===0){
+						for(var i=0;i<31;i++){
+							$scope.days.push(i+1)
+						}
+					}else{
+						for(var i=0;i<30;i++){
+							$scope.days.push(i+1)
+						}
+					}
+				}
+			}
+			if($scope.day>$scope.days.length){
+				$scope.day="1"
 			}
 		}
-	}
-
-$(document).ready(()=>{
-	$("input,textarea").focus(event=>{
-		if($(window).width()<=992){
-			$("body,html").scrollTop($(event.currentTarget).offset().top-75)
+		$scope.submit=function($event){
+			if($($window).width()<991){
+				$('body,html').scrollTop(0)
+			}
+			var now=new Date().getTime()
+			var date=new Date();date.setFullYear(parseInt($scope.year));date.setMonth(parseInt($scope.month)-1);date.setDate(parseInt($scope.day));
+			var age=(new Date(now-date.getTime()).getFullYear()-1970)
+			$("input[name='age']").attr("value",age)
+			var user=sign_up_user($event.currentTarget)
+			if(!user){
+				$event.preventDefault()
+				return
+			}
+			if(age<16){
+				$("div#sign_up_error").html("<div class='alert alert-danger alert-dismissable fade show'><strong>Error: </strong>You do not meet age requirements to create an account on the site.<button class='close' style='line-height:0.83;outline:none;' data-dismiss='alert'><span>&times;</span></button></div>")
+				$event.preventDefault()
+			}
 		}
-	})
-})
+		$scope.day=new Date().getDate().toString()
+		$scope.year=new Date().getFullYear().toString()
+		$scope.month=(new Date().getMonth()+1).toString()
+	}
