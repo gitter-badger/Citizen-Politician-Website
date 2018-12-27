a8dfa5493c56811bf91e91b91128febe199d9f26
Dopesky <oboke69@gmail.com>
2018-12-27 12:26:26 +0300

    removed db error

--- application/models/Sendvalidationphone.php
+++ application/models/Sendvalidationphone.php
@@ -1,36 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sendvalidationemail extends CI_Model {
+class Sendvalidationphone extends CI_Model {
 	private $code;
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->library("email");
+		$this->load->library("phone");
 	}
 
-	public function index($username,$email){
-		if($this->formatter->checkEmail($email)){
+	public function index($username,$phone){
+		if(is_numeric($phone)){
 			$this->code=$this->formatter->generate_random_code(random_int(4,6));
 			if($this->log_event($username)){
-				if($this->email->send_email($email,"Welcome to Mwananchi","Glad to have you on board. To kick off, please use this code to validate your email.<br><br>$this->code<br><br>",$username)){
+				if($this->phone->send_text("Your Mwananchi verification code is $this->code",$phone)){
 					return true;
 				}
-				return $this->email->mailer->ErrorInfo;
 			}
-			return false;
 		}
+		return false;
 	}
 
 	private function log_event($email){
-		if($this->db->query("update emailgetcredentials set used=1 where userEmail=? and type='email'",$email)){
-			return $this->db->query("insert into emailgetcredentials(userEmail,passCode,type) values (?,?,'email')",array($email,$this->code));
+		if($this->db->query("update emailgetcredentials set used=1 where userEmail=? and type='phone'",$email)){
+			return $this->db->query("insert into emailgetcredentials(userEmail,passCode,type) values (?,?,'phone')",array($email,$this->code));
 		}
 		return false;
 	}
 
 	private function get_log($email){
-		return $this->db->query("select * from emailgetcredentials where userEmail=? and type='email' and used=0",$email)->result();
+		return $this->db->query("select * from emailgetcredentials where userEmail=? and type='phone' and used=0",$email)->result();
 	}
 
 	public function verifyCode($code,$email){
@@ -42,7 +41,7 @@ class Sendvalidationemail extends CI_Model {
 			if($value->passCode===$code){
 				if($this->db->query("update emailgetcredentials set used=1 where eventID=?",$value->eventID)){
 					$userdata=$this->session->userdata('basic_data');
-					$userdata['email_verified']=1;
+					$userdata['phone_verified']=1;
 					$this->session->set_userdata('basic_data',$userdata);
 					return "valid";
 				}else{
