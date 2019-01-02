e4a63231f998b248bb1050ea9a020d28f5e57481
Dopesky <oboke69@gmail.com>
2019-01-02 11:57:56 +0300

    Testing voice call

--- application/views/say_validation_code.php
+++ application/views/say_validation_code.php
@@ -5,8 +5,8 @@ require_once APPPATH.'third_party/Twilio/Twilio/autoload.php';
 use Twilio\TwilML\VoiceResponse;
 
 $response=new VoiceResponse();
-$response->say("$code.");
-$response->say("$code.");
+$response->say("i am kevin");
+$response->say("i am kevin");
 
 echo $response;
 ?>
