751d0a02ef5a032f7eb7f1799a3d1419877b17d3
Dopesky <oboke69@gmail.com>
2019-01-02 12:31:26 +0300

    Testing voice call

--- application/controllers/TwiML.php
+++ application/controllers/TwiML.php
@@ -9,7 +9,7 @@ class TwiML extends CI_Controller {
 
 	public function index($code){
 		$data['message']="Please input this code on the Mwananchi site or app.";
-		$data['code']=implode(", ",explode($code));
+		$data['code']=implode(", ",str_split($code));
 		$this->load->view("say_validation_code",$data);
 	}
 }
