cb0ee67df060f34e9d07b5bd3a1a2d8f1e4b57d2
Dopesky <oboke69@gmail.com>
2019-01-07 01:36:30 +0300

    Changed message for code calls before code is said

--- application/controllers/TwiML.php
+++ application/controllers/TwiML.php
@@ -8,7 +8,7 @@ class TwiML extends CI_Controller {
 	}
 
 	public function index($code){
-		$data['message']="Please input this code on the site or app.";
+		$data['message']="Please input this code.";
 		$data['code']=implode(", ",str_split($code));
 		$this->load->view("say_validation_code",$data);
 	}
