fe0e115ed7dd41054607c63d26a8f0703a089acc
Dopesky <oboke69@gmail.com>
2019-01-07 01:39:50 +0300

    Changed voice to alice and added pauses

--- application/views/say_validation_code.php
+++ application/views/say_validation_code.php
@@ -5,13 +5,13 @@ require_once APPPATH.'third_party/Twilio/Twilio/autoload.php';
 use Twilio\TwiML\VoiceResponse;
 
 $response=new VoiceResponse();
-$response->say("$message");
+$response->say("$message", ['voice'=>'alice','language'=>'en-GB']);
 $response->pause(['length'=>2]);
-$response->say("$code.");
+$response->say("$code.", ['voice'=>'alice','language'=>'en-GB']);
 $response->pause(['length'=>2]);
-$response->say("$message");
+$response->say("$message", ['voice'=>'alice','language'=>'en-GB']);
 $response->pause(['length'=>2]);
-$response->say("$code.");
+$response->say("$code.", ['voice'=>'alice','language'=>'en-GB']);
 $response->pause(['length'=>2]);
 
 echo $response;
