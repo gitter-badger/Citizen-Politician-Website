df0b17c2f340168fb962252c14fcbe3d7e2f736e
Dopesky <oboke69@gmail.com>
2019-01-21 16:49:39 +0300

    Testing build pass

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -29,7 +29,7 @@ if ( ! class_exists('vfsStream') && file_exists(PROJECT_BASE.'vendor/autoload.ph
 
 // Define CI path constants to VFS (filesystem setup in CI_TestCase::setUp)
 defined('BASEPATH') OR define('BASEPATH', PROJECT_BASE.'system/');
-defined('APPPATH') OR define('APPPATH', PROJECT_BASE.'application/'));
+defined('APPPATH') OR define('APPPATH', PROJECT_BASE.'application/');
 defined('VIEWPATH') OR define('VIEWPATH', APPPATH.'views/');
 defined('ENVIRONMENT') OR define('ENVIRONMENT', 'development');
 
