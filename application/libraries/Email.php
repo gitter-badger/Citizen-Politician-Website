e9cfc5f1c4833cdff8da7a0660ca12e19b48065a
Dopesky <oboke69@gmail.com>
2019-01-25 20:16:52 +0300

    Removed unnecessary require statements

--- application/libraries/Email.php
+++ application/libraries/Email.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once (APPPATH."third_party/SendGrid/sendgrid-php.php");
 class Email {
 	private $CI;
 
--- application/libraries/Phone.php
+++ application/libraries/Phone.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once (APPPATH."third_party/Twilio/Twilio/autoload.php");
 use Twilio\Rest\Client;
 class Phone {
 	private $CI;
